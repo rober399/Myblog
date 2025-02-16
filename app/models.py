ec8c9cd723434837bf288583d21d6c5e6a99f1d1
Roberto <roberore99@gmail.com>
2025-02-15 18:40:34 -0600

    Blogv3

--- app/models.py
+++ app/models.py
@@ -3,9 +3,9 @@ from app import db
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f'<Post {self.title}>'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, url_for
+from app import app, db
+from app.models import Post
+from app.forms import PostForm
 
 @app.route('/')
-def home():
-    return render_template('base.html')
+@app.route('/index')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been added.')
+        return redirect(url_for('index'))
+    return render_template('add.html', title='Add Post', form=form)
